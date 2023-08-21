2971da49706bd6b2d268cc4d815ee5c1b64446bf
Ksenia <ksyu.arfanova.02@mail.ru>
2023-08-21 18:00:22 +0300

    epoxy

--- project/epoxyd/epoxydsmola/views.py
+++ project/epoxyd/epoxydsmola/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project
-
+from .forms import ProjectF
 
 def projects(request):
     pr = Project.objects.all()
@@ -13,4 +13,13 @@ def project(request, pk):
     project_obj = Project.objects.get(id=pk)
     return render(request, "projects/single-project.html", {'project': project_obj})
 
+def create_project(request):
+    form = ProjectF()
 
+    if request.method == 'POST':
+        form = ProjectF(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    return render(request, 'projects/form-template.html', {'form': form})
