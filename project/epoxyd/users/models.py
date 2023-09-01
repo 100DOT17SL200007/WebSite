db1b687f5408d5f312e2aa343065dd70c082cac9
Ksenia <ksyu.arfanova.02@mail.ru>
2023-09-01 09:58:40 +0300

    login

--- project/epoxyd/users/models.py
+++ project/epoxyd/users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -27,3 +29,6 @@ class Skill(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
